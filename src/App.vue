7ab20c6a4daee2e4afa3a2f6eb21c7e9698acee5
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-08 23:21:55 +0530

    header footer added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div id="nav">
+      <Navbar />
+    </div>
+  
+    <div style="min-height: 60vh">
+      <router-view v-if="products && categories"
+      :baseURL="baseURL"
+      :products="products"
+      :categories="categories"
+      @fetchData = "fetchData">
+      
+      </router-view>
+  </div>
+
+  <Footer />
+  </div>
 </template>
 
+<script>
+const axios = require('axios');
+import Footer from "./components/FooterView.vue"
+import Navbar from "./components/NavbarView.vue"
+
+export default {
+  data() {
+    return {
+      baseURL :   "http://localhost:8080/",
+      products : null,
+      categories : null
+    }
+  },
+ components : {Footer, Navbar },
+  methods : {
+    async fetchData() {
+      // fetch products
+      await axios.get(this.baseURL + "product/")
+       .then(res => this.products = res.data)
+              .catch(err => console.log(err))
+  await axios.get(this.baseURL + "category/")
+              .then(res => this.categories = res.data)
+              .catch(err => console.log(err))
+ }
+  },
+  mounted() {
+    this.fetchData();
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
