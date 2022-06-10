e5dc88bddf192728bbb258a75a8cac6a8ecf55ea
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-10 21:51:28 +0530

    navbar modified to add signup/signin/login state

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-
     <router-link class="navbar-brand" :to="{name : 'HomeView'}">
+      <img id="logo" alt="logo" src="../assets/icon.png" />
     </router-link>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -10,8 +10,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <form class="form-inline ml-auto mr-auto">
+      <form class="form-inline ml-auto mr-auto">
         <div class="input-group">
           <input size="100" type="text" class="form-control" placeholder="Search Items" aria-label="Username" aria-describedby="basic-addon1">
           <div class="input-group-prepend">
@@ -23,30 +22,63 @@
           </div>
         </div>
       </form>
-
-            <div>
-            <router-link style="color: white" :to="{name : 'AdminCategory'}">
-                Category
-            </router-link>
-            <router-link style="color: white" :to="{name : 'AdminProduct'}">
-                Products
-            </router-link>
-        </div>
     </div>
+   
+    <li class="nav-item dropdown">
+      <a class="nav-link text-light dropdown-toggle" 
+         href="#" 
+         id="navbarDropdownAdmin" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Admin
+      </a>
+
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownAdmin">
+          <router-link class="dropdown-item" :to="{name : 'AdminCategory'}">Category</router-link>
+          <router-link class="dropdown-item" :to="{name : 'AdminProduct'}">Products</router-link>
+      </div>
+
+    </li>
+
+    <li class="nav-item dropdown">
+      <a class="nav-link text-light dropdown-toggle" href="#" id="navbarDropdown" 
+         role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Accounts
+      </a>
+ 
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <router-link class="dropdown-item" v-if="!token" :to="{name: 'SignIn'}">Log In</router-link>
+          <router-link class="dropdown-item" v-if="!token" :to="{name: 'SignUp'}">Sign Up</router-link>
+          <a class="dropdown-item" v-if="token" href="#" @click="signout">Sign Out</a>
+      </div>
+    </li>
+
   </nav>
 
 </template>
 
 <script>
+import swal from 'sweetalert';
 export default {
   name : "NavbarView",
   data() {
-    return {
-      token: null
-    }
+      return {
+          token: null
+      };
   },
   methods: {
+      signout() {
+          localStorage.removeItem('token');
+          this.token = null;
+          this.$router.push({name:'Home'});
+          swal({
+              text: "Logged you out. Visit Again",
+              icon: "success",
+              closeOnClickOutside: false,
+          });
+      }
   },
+  mounted() {
+    this.token = localStorage.getItem('token');
+  }
 }
 </script>
 
